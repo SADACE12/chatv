3b09ae73d1f2d5092aeead9497699d8ce1dbcf74
Almas <amanalmasbek123@gmail.com>
2026-04-02 18:26:28 +0500

    БОЛЬШОЙ ФИКС

--- chatv/android/app/src/main/kotlin/com/example/chatv/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.chatv
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- chatv/lib/data/app_data.dart
+++
@@ -1,4 +0,0 @@
-class AppData {
-  // Локальная база данных для текущей сессии
-  static Map<String, int> activeClans = {};
-}
